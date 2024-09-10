7356342679ee429657806e2784d5f847e4c4eb3f
Vadim Kantorov <vadimkantorov@gmail.com>
2024-09-10 20:34:49 +0200

    ...

--- libarchivepreload.c
+++ libarchivepreload.c
@@ -162,7 +162,7 @@ struct packfs_context* packfs_ensure_context(const char* path)
         
         packfs_ctx.disabled = (packfs_archive_filename != NULL && strlen(packfs_archive_filename) > 0) ? 0 : 1;
 #ifdef PACKFS_LOG 
-            fprintf(stderr, "packfs: disabled: %d, %s, prefix: %s\n", packfs_ctx.disabled, packfs_archive_filename, packfs_ctx->packfs_archive_prefix);
+        fprintf(stderr, "packfs: disabled: %d, %s, prefix: %s\n", packfs_ctx.disabled, packfs_archive_filename, packfs_ctx.packfs_archive_prefix);
 #endif
         
         struct archive *a = archive_read_new();
