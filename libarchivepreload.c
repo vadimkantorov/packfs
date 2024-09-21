3442c292abdd761915a1bc25760f69553e68fbe7
Vadim Kantorov <vadimkantorov@gmail.com>
2024-09-21 02:00:28 +0200

    ...

--- libarchivepreload.c
+++ libarchivepreload.c
@@ -443,6 +443,8 @@ int packfs_close(struct packfs_context* packfs_ctx, int fd)
     {
         if(packfs_ctx->packfs_filefd[k] == fd)
         {
+            if(packfs_ctx->packfs_fileisdir[k]) return -1;
+
             int res = (!packfs_ctx->packfs_fileisdir[k]) ? packfs_ctx->orig_fclose(packfs_ctx->packfs_fileptr[k]) : 0;
             packfs_ctx->packfs_fileisdir[k] = 0;
             packfs_ctx->packfs_filefd[k] = 0;
