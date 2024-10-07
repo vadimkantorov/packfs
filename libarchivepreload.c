726d38d1fbf048d76b05b92073425f8e1435e29b
Vadim Kantorov <vadimkantorov@gmail.com>
2024-10-07 15:18:56 +0200

    ...

--- libarchivepreload.c
+++ libarchivepreload.c
@@ -1114,6 +1114,8 @@ int closedir(DIR* stream)
 
 int packfs_dup(struct packfs_context* packfs_ctx, int oldfd, int newfd)
 {
+    return oldfd;
+    /*
     int K = -1;
     if(oldfd >= 0 && packfs_filefd_min <= oldfd && oldfd < packfs_filefd_max)
     {
@@ -1126,7 +1128,6 @@ int packfs_dup(struct packfs_context* packfs_ctx, int oldfd, int newfd)
             }
         }
     }
-    
     for(size_t k = 0; K >= 0 && k < packfs_filefd_max - packfs_filefd_min; k++)
     {
         int fd = packfs_filefd_min + k;
@@ -1134,11 +1135,11 @@ int packfs_dup(struct packfs_context* packfs_ctx, int oldfd, int newfd)
         {
             packfs_ctx->packfs_fileisdir[k] = packfs_ctx->packfs_fileisdir[K];
             packfs_ctx->packfs_filefd[k]    = packfs_ctx->packfs_filefd[K];
-            packfs_ctx->packfs_fileptr[k]   = NULL; //TODO: how to dup fmemopen-produced state?
             packfs_ctx->packfs_filesize[k]  = packfs_ctx->packfs_filesize[K];
             packfs_ctx->packfs_fileino[k]   = packfs_ctx->packfs_fileino[K];
             packfs_ctx->packfs_dirent[k]    = (struct dirent){0};
-            
+            packfs_ctx->packfs_fileptr[k]   = NULL; //TODO: how to dup fmemopen-produced state?
+
             if(packfs_ctx->packfs_fileisdir[k])
             {
                 packfs_ctx->packfs_dirent[k] = packfs_ctx->packfs_dirent[K];
@@ -1147,9 +1148,8 @@ int packfs_dup(struct packfs_context* packfs_ctx, int oldfd, int newfd)
             return fd;
         }
     }
-
     return -1;
-    //return oldfd;
+    */
 }
 
 int fcntl(int fd, int action, ...)
@@ -1206,9 +1206,9 @@ int fcntl(int fd, int action, ...)
         int res = (action == F_DUPFD || action == F_DUPFD_CLOEXEC) ? packfs_dup(packfs_ctx, fd, intarg) : -1;
         if(res >= -1)
         {
-#ifdef PACKFS_LOG
+//#ifdef PACKFS_LOG
             fprintf(stderr, "packfs: Fcntl(%d, %d, %d/%p) == %d\n", fd, action, intarg, ptrarg, res);
-#endif
+//#endif
             return res;
         }
     }
