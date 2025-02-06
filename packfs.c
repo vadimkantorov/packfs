10d3be72608fa83675825ecdc7a1ce2d88f5cf38
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-06 17:53:06 +0100

    ...

--- packfs.c
+++ packfs.c
@@ -429,7 +429,7 @@ struct dirent* packfs_readdir(void* stream)
         size_t entrypath_len = strlen(entrypath);
         int entryisdir = entrypath_len > 0 && entrypath[entrypath_len - 1] == packfs_sep;
         
-        fprintf(stderrr, "packfs_readdir1: '%s' '%s'\n", dir_entry_name, entrypath);
+        fprintf(stderr, "packfs_readdir1: '%s' '%s'\n", dir_entry_name, entrypath);
 
         if(i > (size_t)dir_entry->d_ino && packfs_indir(dir_entry_name, entrypath))
         {
@@ -447,7 +447,7 @@ struct dirent* packfs_readdir(void* stream)
                 const char* last_slash = strrchr(entrypath, packfs_sep);
                 strcpy(dir_entry->d_name, last_slash != NULL ? (last_slash + 1) : entrypath);
             }
-            fprintf(stderrr, "packfs_readdir2: '%s' '%s'\n", dir_entry->d_name, entryisdir);
+            fprintf(stderr, "packfs_readdir2: '%s' '%s'\n", dir_entry->d_name, entryisdir);
             dir_entry->d_type = entryisdir ? DT_DIR : DT_REG;
             dir_entry->d_ino = (ino_t)i;
             return dir_entry;
